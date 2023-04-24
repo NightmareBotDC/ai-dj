09888e5c3477c4b74c1589068b9fd751a06ecfc7
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-24 17:06:47 -0500

    Update +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="m-4 p-15 text-base text-white font-bold">
+   <h2>AzidoDJ</h2>
+   <p>Welcome to your personalized Artificial Intelligence based Music DJ. Powered by <span class="text-green-900">Spotify</span></p>
+</div>
