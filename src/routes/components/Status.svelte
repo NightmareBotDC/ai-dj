3e50628f577521d78beeeefa670441d02cf31b14
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-25 08:49:08 -0500

    Fixed status component

--- src/routes/components/Status.svelte
+++ src/routes/components/Status.svelte
@@ -14,23 +14,27 @@
 	});
 </script>
 
-{#if status}
-	<div class="p-2 bg-gradient-to-r from-gray-700 via-gray-900 to-black inline-block rounded-md">
-		<div class="flex items-center justify-center">
-			{#if status.page.status === 'UP'}
-				<div class="p-2 bg-green-500 rounded-lg" />
-				<h2 class="pl-2 text-green-500 font-bold text-center">All services are online!</h2>
-			{/if}
+{#if status && !status.error}
+	<center>
+		<div class="p-2 bg-gradient-to-r from-gray-700 via-gray-900 to-black inline-block rounded-md">
+			<div class="flex items-center justify-center">
+				{#if status.page.status === 'UP'}
+					<div class="p-2 bg-green-500 rounded-lg" />
+					<h2 class="pl-2 text-green-500 font-bold text-center">All services are online!</h2>
+				{/if}
 
-			{#if status.page.status === 'HASISSUES'}
-				<div class="p-2 bg-red-500 rounded-lg" />
-				<h2 class="pl-2 text-red-500 font-bold text-center">Some services are having issues!</h2>
-			{/if}
+				{#if status.page.status === 'HASISSUES'}
+					<div class="p-2 bg-red-500 rounded-lg" />
+					<h2 class="pl-2 text-red-500 font-bold text-center">Some services are having issues!</h2>
+				{/if}
 
-			{#if status.page.status === 'UNDERMAINTENANCE'}
-				<div class="p-2 bg-white rounded-lg" />
-				<h2 class="pl-2 text-white font-bold text-center">Some services are under maintenance!</h2>
-			{/if}
+				{#if status.page.status === 'UNDERMAINTENANCE'}
+					<div class="p-2 bg-white rounded-lg" />
+					<h2 class="pl-2 text-white font-bold text-center">
+						Some services are under maintenance!
+					</h2>
+				{/if}
+			</div>
 		</div>
-	</div>
+	</center>
 {/if}
