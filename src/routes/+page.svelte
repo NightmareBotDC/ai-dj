04fc2a0c83ccdf36ac41f145311625ae1611f178
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-26 17:24:26 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,11 +37,11 @@
         {#if accountConnected("Spotify") }
            <h1 class="text-white font-bold m-4">Welcome back, {data.user.Username}! Your Spotify account is already linked to your Azidoazide profile. Start your enhanced music listening experience now!</h1>
            <button
-              class="m-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="ml-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               on:click={startExperience}
-           >Start your Experience <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+           ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
-</svg></button>
+</svg> Play Now!</button>
         {:else}
 	   <h1 class="text-white font-bold m-4">Hello, {data.user.Username}. It seems you don't have a Spotify account linked with your Azidoazide profile. Please go to <span class="text-spotify font-bold">Link Account</span> to start using AzidoDJ today!</h1>
         {/if}
