1d9f2a638a9a6f9fd6fdd34135fc13fdedffbb1d
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-26 17:38:53 -0500

    Create +page.svelte

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+   let Loading: Boolean = true;
+   export let data: any;
+
+   const isAccountConnected = (p) => { return data.user.Connections.find((e) => e.service === p); };
+</script>
+
+{#if Loading}
+   <div id="loading" class="text-base text-white font-bold">
+<div class="text-center">
+	<p id="song">🎶</p>
+</div>
+
+<div class="p-5" />
+
+<p class="text-base text-white text-center font-bold">
+   Loading your personalized DJ experience...
+</p>
+
+<style>
+	#song {
+		animation: bobble 2s infinite;
+		font-size: 60px;
+		margin-top: 0;
+	}
+
+	@keyframes bobble {
+		0% {
+			transform: translateY(10px);
+		}
+		50% {
+			transform: translateY(40px);
+		}
+		100% {
+			transform: translateY(10px);
+		}
+	}
+</style>
+   </div>
+{/if}
