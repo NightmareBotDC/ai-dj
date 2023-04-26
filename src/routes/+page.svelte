aa260e396cb6a95479726defd301aca16fb45cdc
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-26 17:18:50 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
         export let data: any;
 
         const accountConnected = (d) => { return data.user.Connections.find((i) => i.service === d); };
+        const startExperience = () => { return window.location.href = "/app"; };
 </script>
 
 <Meta
@@ -35,6 +36,10 @@
 {#if data.user}
         {#if accountConnected("Spotify") }
            <h1 class="text-white font-bold m-4">Welcome back, {data.user.Username}! Your Spotify account is already linked to your Azidoazide profile. Start your enhanced music listening experience now!</h1>
+           <button
+              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              on:click={startExperience}
+           >Start your Experience =></button>
         {:else}
 	   <h1 class="text-white font-bold m-4">Hello, {data.user.Username}. It seems you don't have a Spotify account linked with your Azidoazide profile. Please go to <span class="text-spotify font-bold">Link Account</span> to start using AzidoDJ today!</h1>
         {/if}
