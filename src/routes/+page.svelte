371c43a9665327ed4ad72c8e63c464fb68fcaf67
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-26 17:21:15 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,7 +37,7 @@
         {#if accountConnected("Spotify") }
            <h1 class="text-white font-bold m-4">Welcome back, {data.user.Username}! Your Spotify account is already linked to your Azidoazide profile. Start your enhanced music listening experience now!</h1>
            <button
-              class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="m-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               on:click={startExperience}
            >Start your Experience =></button>
         {:else}
