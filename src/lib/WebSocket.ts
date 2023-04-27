0de52e26231658df6ce4c43f24eccad00220e615
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-27 16:39:09 -0500

    Update WebSocket.ts

--- src/lib/WebSocket.ts
+++ src/lib/WebSocket.ts
@@ -1,3 +1,3 @@
-import io from "socket.io-client/dist/socket.io.js";
+import { io } from "socket.io-client/dist/socket.io.js";
 
-export const Socket: any = io("wss://api.azidoazide.xyz");
+export const Socket = io("wss://api.azidoazide.xyz");
