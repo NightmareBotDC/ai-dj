2c1d56c8891247367d0a77e6068ae7ea3259333c
selectdev <66577268+selectdev@users.noreply.github.com>
2023-04-29 17:12:03 -0500

    Update +page.svelte

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -99,6 +99,41 @@
 				];
 			}
 
+                        if ("onSpotifyWebPlaybackSDKReady" in window) {
+                            window.onSpotifyWebPlaybackSDKReady = () => {
+                               const token = isAccountConnected('Spotify').accessToken;
+                               const player = new Spotify.Player({
+                                  name: 'AzidoDJ',
+                                  getOAuthToken: cb => { cb(token); },
+                                  volume: 0.5
+                               });
+      
+                               // Ready
+                               player.addListener('ready', ({ device_id }) => {
+                                   console.log('Ready with Device ID', device_id);
+                               });
+
+                               // Not Ready
+                               player.addListener('not_ready', ({ device_id }) => {
+                                   console.log('Device ID has gone offline', device_id);
+                               });
+
+                               player.addListener('initialization_error', ({ message }) => {
+                                   console.error(message);
+                               });
+
+                               player.addListener('authentication_error', ({ message }) => {
+                                   console.error(message);
+                               });
+
+                               player.addListener('account_error', ({ message }) => {
+                                   console.error(message);
+                               });
+
+                              player.connect();
+                          }
+                        }
+
 			setTimeout(() => {
 				if (WS.connected) {
 					EventLogs = EventLogs.concat({
